725df3322f15bc2d7cd6617cf12bf47285a59d7e
Pablo Alves <PabloHAlves99@gmail.com>
2023-07-26 20:33:14 -0300

    Reajuste

--- Python USP/exercicios/primeiro.py
+++ Python USP/exercicios/primeiro.py
@@ -2,7 +2,7 @@ a = 20 ** 9
 b = "Olá, mundo. Livramos da maldição"
 
 print (b)
-print ("O calculo de 20 ** 9 é:",a )
+print ("O calculo de 20 ** 9 é:", a )
 print ("A primeira frase desse programa contém : ", len(b) ,"caracteres.")
 
 con = str(a)
