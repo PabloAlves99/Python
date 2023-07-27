36c06e8a2f342b42a4698fd6e5769e7f13a08564
Pablo Alves <PabloHAlves99@gmail.com>
2023-07-27 18:43:30 -0300

    Update listaDeSeries.py

--- Python USP/listaDeSeries.py
+++ Python USP/listaDeSeries.py
@@ -1,18 +1,40 @@
 def adicionarSerie():
-    series = []
+    series = [] 
+    max_series = 20 
     
     while True:
-        serie = input("\nDigite em ordem de preferencia as séries que você mais gosta (ou 'sair' para encerrar): ")        
-        if serie.lower() == "sair":
-            break       
-        series.append(serie)
+        serie = input("\nDigite em ordem de preferencia as séries que você mais gosta (ou 'sair' para encerrar): ")      
+         
+        if serie.lower() == "sair": # lower() tornou tudo minusculo para verificar
+            break 
+         
+        if not serie: # Quando não recebe nada, fica negativo automaticamente
+            print("Por favor, digite o nome de uma série válida.")
+            continue # Volta no inicio do while
+        
+        if serie.lower() in [s.lower() for s in series]: # Se serie(em minusculo) em [ S.lower recebe a serie dentro do for em minusculo e o "s" recebe oque tem dentro da lista] 
+            # Se o serie antes do "in" for igual o s depois do "in"...
+            print("Essa série já foi adicionada. Digite outra série ou 'sair' para encerrar.")
+        else:
+            series.append(serie) # Adicionar na lista
+            if len(series) >= max_series:
+                print(f"Limite máximo de séries ({max_series}) atingido. Encerrando adição de séries.")
+                break
+            
+        # Armazenamento permanente em arquivo
+   # with open("series_preferidas.txt", "w") as arquivo:
+       # for serie in series:
+          #  arquivo.write(serie + "\n")
         
     return series
 
 def exibirLista(series):
+    
     if series:
         print("\nAs suas séries preferidas são: ")
-        for indice, serie in enumerate(series):
+        
+        for indice, serie in enumerate(series): 
+            serie = serie.capitalize() # Capitalize deixa a primeira letra maiúscula
             print(f"Em {indice + 1}º lugar: {serie}")
     else:
         print("\nNenhuma série foi adicionada")
