b4665f65fa428f8e523c8b5130d843047e0f46d1
Pablo Alves <PabloHAlves99@gmail.com>
2023-07-28 21:13:31 -0300

    Alteração nas pastas

--- Python USP/caixaEletronico.py
+++ Python USP/caixaEletronico.py
@@ -16,8 +16,11 @@ nota10 = resto50 // 10
 resto10 = resto50 % 10
 nota5 = resto10 // 5
 
+sobra = resto10 % 5
+
 print ("O caixa eletrônico vai te entregar as seguintes notas:")
 print(nota100, "notas de R$100")
 print(nota50, "notas de R$50")
 print(nota10, "notas de R$10")
 print(nota5, "notas de R$5")
+print(f'\nSobrou R${sobra:.2f}')
