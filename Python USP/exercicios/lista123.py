59cc797963ff6f7e7831404bbcb8d2a4cf0e0479
Pablo Alves <PabloHAlves99@gmail.com>
2023-08-08 14:48:03 -0300

    New

---
+++ Python USP/exercicios/lista123.py
@@ -0,0 +1,14 @@
+lista1 = {"a", "b", "c",
+          "d", "e"}
+
+lista2 = {"a", "b", "c",
+          "d", "e"}
+
+lista3 = {"a", "d"}
+
+print(lista1 is lista3)
+print(lista3 is lista1)
+print(lista1 is lista2)
+print(lista1 == lista3)
+print(lista3 == lista1)
+print(lista1 == lista2)
