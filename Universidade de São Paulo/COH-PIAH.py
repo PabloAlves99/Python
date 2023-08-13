0d6d76d38c5607a0864994e80f1d898d436f5f15
Pablo Alves <PabloHAlves99@gmail.com>
2023-08-13 16:26:45 -0300

    Update COH-PIAH.py

--- Universidade de São Paulo/COH-PIAH.py
+++ Universidade de São Paulo/COH-PIAH.py
@@ -2,10 +2,10 @@ import re
 
 def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
-    print("Bem-vindo ao detector automático de COH-PIAH.")
+    print("\nBem-vindo ao detector automático de COH-PIAH.")
     print("Informe a assinatura típica de um aluno infectado:")
 
-    wal = float(input("Entre o tamanho médio de palavra: "))
+    wal = float(input("\nEntre o tamanho médio de palavra: "))
     ttr = float(input("Entre a relação Type-Token: "))
     hlr = float(input("Entre a Razão Hapax Legomana: "))
     sal = float(input("Entre o tamanho médio de sentença: "))
