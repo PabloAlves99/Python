8575d9c89a4343c9c1d89b8fc736c75cc2038d0f
Pablo Alves <PabloHAlves99@gmail.com>
2023-08-14 22:52:55 -0300

    Create letraMaisVezes.py

---
+++ Udemy/Python(com projetos reais)/letraMaisVezes.py
@@ -0,0 +1,23 @@
+frase = 'O python é uma linguem de programação ' \
+        'multiparadigma. ' \
+        'Python foi criado por Guido Van Rossum.'
+
+i = 0
+maior = 0
+letra = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+    
+    if letra_atual == ' ':
+        i += 1
+        continue
+    
+    apareceu = frase.count(letra_atual)
+    
+    if maior < apareceu:
+        maior = apareceu
+        letra = letra_atual
+    i += 1
+
+print(f'A letra que apareceu mais vezes foi {letra} que apareceu {maior}x')
