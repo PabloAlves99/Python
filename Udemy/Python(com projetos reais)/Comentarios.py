9bf4494f21125a2c562785c0ca150e4576ab5997
Pablo Alves <PabloHAlves99@gmail.com>
2023-08-20 22:55:13 -0300

    Comentários úteis

---
+++ Udemy/Python(com projetos reais)/Comentarios.py
@@ -0,0 +1,27 @@
+# *** LISTAS ***
+
+# append - Adiciona um item ao final da lista
+# insert  - Adiciona um item no índice escolhido
+# pop - Remove do final ou do índice escolhido
+# del - apaga um indice
+# clear - Limpa a lista
+# extend - estende a lista
+# + - concatena a lista
+
+# *** string ***
+
+# split() retorna uma lista com as palavras do texto
+# strip() tira os espaços no inicio e no fim
+# lstrip() tira o espaço do inicio
+# rstrip() tira o espaço do final
+
+""" join() une as strings ex: 
+
+lista_aleatoria = ['texto1 aleatório', 'texto2 aleatório']
+
+i = ', '.join(lista_aleatória)
+
+print(i)
+terminal...
+texto1 aleatório, tesxto2 aleatório
+"""
--- Udemy/Python(com projetos reais)/ComentariosLista.py
+++
@@ -1,7 +0,0 @@
-# append - Adiciona um item ao final da lista
-# insert  - Adiciona um item no índice escolhido
-# pop - Remove do final ou do índice escolhido
-# del - apaga um indice
-# clear - Limpa a lista
-# extend - estende a lista
-# + - concatena a lista
