9c3293e15f4ee4ea5bad15ca65e7b8291d9f7d4d
Pablo Alves <PabloHAlves99@gmail.com>
2023-08-20 23:38:15 -0300

    Update Comentarios.py

--- Udemy/Python(com projetos reais)/Comentarios.py
+++ Udemy/Python(com projetos reais)/Comentarios.py
@@ -1,22 +1,23 @@
-# *** LISTAS ***
+"""
+*** LISTAS ***
 
-# append - Adiciona um item ao final da lista
-# insert  - Adiciona um item no índice escolhido
-# pop - Remove do final ou do índice escolhido
-# del - apaga um indice
-# clear - Limpa a lista
-# extend - estende a lista
-# + - concatena a lista
+append - Adiciona um item ao final da lista
+insert  - Adiciona um item no índice escolhido
+pop - Remove do final ou do índice escolhido
+del - apaga um indice
+clear - Limpa a lista
+extend - estende a lista
++ - concatena a lista
 
-# *** string ***
+*** string ***
 
-# split() retorna uma lista com as palavras do texto
-# strip() tira os espaços no inicio e no fim
-# lstrip() tira o espaço do inicio
-# rstrip() tira o espaço do final
+count() retorna quantas vezes tal coisa aparece
+split() retorna uma lista com as palavras do texto
+strip() tira os espaços no inicio e no fim
+lstrip() tira o espaço do inicio
+rstrip() tira o espaço do final
 
-""" join() une as strings ex: 
-
+join() une as strings ex: 
 lista_aleatoria = ['texto1 aleatório', 'texto2 aleatório']
 
 i = ', '.join(lista_aleatória)
