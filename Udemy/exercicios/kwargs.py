e7c5b916935299cab005094e9e4b03e8b29c03f4
Pablo Alves <PabloHAlves99@gmail.com>
2023-09-08 19:08:11 -0300

    Create kwargs.py

---
+++ Udemy/exercicios/kwargs.py
@@ -0,0 +1,23 @@
+pessoa = {
+    'nome': 'Pablo',
+    'sobrenome': 'Alves',
+}
+
+dados = {
+    'idade': 19,
+    'time': 'cruzeiro',
+}
+
+pessoaEDados = {**pessoa, **dados}
+# print(pessoaEDados)
+
+def mostrarNomeados(*args, **kwargs):
+    print(f'Não nomeados {args}\n')
+    # print(f'Nomeados {kwargs}\n')
+    
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+        
+# mostrarNomeados(1, 2, 3, 'qualquer', nome='Henrique', pais='Brasil', time='Real Madrid')
+
+mostrarNomeados(**pessoaEDados)
