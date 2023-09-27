42eea1ac8f91e0365c72f4547b319b6d5a9271ef
Pablo Alves <PabloHAlves99@gmail.com>
2023-09-26 23:15:07 -0300

    addQuickSort

---
+++ Estudos/Outros/quicksort.py
@@ -0,0 +1,16 @@
+import random
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivo = array[0]
+        menor = [i for i in array[1:] if i <= pivo]
+        bigger = [i for i in array[1:] if i > pivo]
+    return quicksort(menor) + [pivo] + quicksort(bigger)
+
+lista = random.sample(range(1,1001), 500)
+
+print(quicksort(lista))
+
+# Usando o sorted(lista), o tempo de execução nesse caso é igual
