ecd65a490b9471b185dc1e159231a331220a20b8
Pablo Alves <PabloHAlves99@gmail.com>
2023-10-06 01:20:08 -0300

    new

--- Estudos/Udemy/with.py
+++ Estudos/Udemy/with.py
@@ -41,6 +41,8 @@ caminho_arquivo = 'aula116.txt'
 with open(caminho_arquivo, 'a+') as arquivo:
     arquivo.write('Olá mundo\n')
     arquivo.write('Olá dnv mundo\n')
-    arquivo.writelines(('testando\n', 'testando linha de baixo\n'))
+    arquivo.writelines(
+        ('testando\n', 'testando linha de baixo\n')
+        )
     arquivo.seek(0.0) # Voltar o cursor pro topo para ler o arquivo no comando da proxima linha
     print(arquivo.read())
