0019afdb2eca4e5e3be4d255f71cb9ac17ae98fa
Pablo Alves <PabloHAlves99@gmail.com>
2023-10-06 21:23:08 -0300

    new

---
+++ Estudos/Udemy/aulas/dadosEmJson.py
@@ -0,0 +1,17 @@
+import json
+
+# pessoa = {
+#     'nome': 'Pablo',
+#     'sobrenome': 'Alves',
+#     'idade': 23,
+#     'altura': 1.75,
+#     'time': 'Cruzeiro',
+# }
+
+# with open('pessoas.json', 'w', encoding='utf8') as arquivo:
+#     json.dump(pessoa, arquivo, indent=2)
+
+with open('pessoas.json', 'r', encoding='utf8') as arquivo:
+    pessoa = json.load(arquivo)
+    
+print(pessoa)
