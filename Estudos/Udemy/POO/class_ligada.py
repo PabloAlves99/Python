7de4b90f5ee8b293b7ce2d4872bf2faad760e68b
Pablo Alves <PabloHAlves99@gmail.com>
2023-10-14 00:30:30 -0300

    New

--- Estudos/Udemy/POO/class_ligada.py
+++ Estudos/Udemy/POO/class_ligada.py
@@ -8,7 +8,7 @@
 # Obs.: Um Fabricante pode fabricar vários carros
 # Exiba o nome do carro, motor e Fabricante na tela
 
-class Carro():
+class Carro ():
     def __init__(self, nome):
         self.nome = nome
         self._motor = None
@@ -16,7 +16,7 @@ class Carro():
         
     @property
     def motor(self):
-        return self._motor 
+        return self._motor
     
     @motor.setter
     def motor(self, motor):
@@ -24,7 +24,7 @@ class Carro():
         
     @property
     def fabricante(self):
-        return self._fabricante 
+        return self._fabricante
     
     @fabricante.setter
     def fabricante(self, fabricante):
@@ -33,7 +33,7 @@ class Carro():
 class Motor():
     def __init__(self, nome):
         self.nome = nome
-
+        
 class Fabricante():
     def __init__(self, nome):
         self.nome = nome
@@ -44,7 +44,26 @@ motor = Motor('eletrico')
 i8.fabricante = bmw
 i8.motor = motor
 
-
 print(i8.nome, i8.fabricante.nome, i8.motor.nome)
 
+x4 = Carro('x4')
+motor = Motor('v8')
+x4.fabricante = bmw
+x4.motor = motor
 
+print(x4.nome, x4.fabricante.nome, x4.motor.nome)
+
+x3 = Carro('x3')
+motor = Motor('v8')
+x3.fabricante = bmw
+x3.motor = motor
+
+print(x3.nome, x3.fabricante.nome, x3.motor.nome)
+
+twistter = Carro('twistter')
+motor = Motor('6M')
+honda = Fabricante('Honda')
+twistter.fabricante = honda
+twistter.motor = motor
+
+print(twistter.nome, twistter.fabricante.nome, twistter.motor.nome)
