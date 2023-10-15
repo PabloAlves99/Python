0a14cf1dddf6eacecd0c6e101ed1519bb76602d0
Pablo Alves <PabloHAlves99@gmail.com>
2023-10-15 17:54:29 -0300

    Update log.py

--- Estudos/Udemy/aulas/POO/mixins/log.py
+++ Estudos/Udemy/aulas/POO/mixins/log.py
@@ -1,13 +1,28 @@
 # Abstração
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent
+
 class log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
+    
+    def log_error(self, msg):
+        self._log(f'Error: {msg}')
  
+    def log_success(self, msg):
+        self._log(f'Success: {msg}')
+        
 class logFileMixin(log):
-    def log(self,msg):
+    def _log(self,msg):
         print(msg)
            
+class logPrintMixin(log):
+    def _log(self,msg):
+        print(f'{msg} ({self.__class__.__name__})')
 
 if __name__ == '__main__':
-    x = logFileMixin()
-    x.log('Qualquer coisa')
+    x = logPrintMixin()
+    x.log_error('Qualquer coisa')
+    x.log_success('Que legal')
+    print(LOG_FILE)
