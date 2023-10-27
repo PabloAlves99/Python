212544253d898663e3c9b5a9b2cfdd9cc641ce93
Pablo Alves <PabloHAlves99@gmail.com>
2023-10-26 21:18:42 -0300

    Update adivinhaNumero.py

--- Projetos/adivinhaNumero.py
+++ Projetos/adivinhaNumero.py
@@ -1,10 +1,39 @@
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
-# EM DESENVOLVIMENTO
+from random import randint
+from time import sleep
+import os
+
+
+def limpar_tela():
+    sistema_operacional = os.name
+    if sistema_operacional == 'nt':  # Windows
+        os.system('cls')
+    else:  # Unix/Linux/Mac
+        os.system('clear')
+
+
+def random_game():
+    attempts = []
+    number_random = randint(1, 1000)   
+    attempt = int(input('Tente adivinhar o número: '))
+    
+    if attempt == number_random:
+        print('DE PRIMEIRAAA!!!')
+    attempts.append(attempt)  
+    while attempt != number_random:
+        limpar_tela()
+        if attempt < number_random:
+            print('O número é maior')
+        else:
+            print('O número é menor')   
+        
+        attempt = int(input('Tente adivinhar novamente: '))
+        attempts.append(attempt)
+    
+    limpar_tela()
+    print(f'\t\tParabens, você acertou!!\n\nO número aleatório é: {attempt}.\nVocê acertou com {len(attempts)} tentativas.')
+    print('\nNúmeros tentados:')
+    print(*attempts, sep='\t')
+        
+        
+random_game()
+# print(number_random)
