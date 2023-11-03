db1d418f00eba814687f9f2b5678fe32406ab665
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-03 00:42:58 -0300

    Update contas_bancarias.py

--- Estudos/Udemy/POO/banco/contas_bancarias.py
+++ Estudos/Udemy/POO/banco/contas_bancarias.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class ContaCorrente():
     pass
 
@@ -8,3 +9,33 @@ class ContaPoupanca():
 
 class Conta(ABC, ContaCorrente, ContaPoupanca):
     pass
+
+class Pessoa(ABC):
+    def __init__(self, nome = None, idade= None):
+        self.nome = nome
+        self.idade = idade
+        
+    @property    
+    def criar_pessoa(self):
+        return self.nome, self.idade
+    
+    @criar_pessoa.setter
+    def criar_pessoa(self, _dados):
+        nome, idade = _dados
+        self.nome = nome
+        self.idade = idade
+        return 'Cadastrado com sucesso'
+        
+
+class Cliente(Pessoa, Conta):
+    def __init__(self, nome=None, idade=None, conta=None):
+        super().__init__(nome, idade)
+        self.conta = conta       
+
+        
+pablo = Cliente()
+print(pablo.__dict__)
+
+pablo.criar_pessoa = 'Pablo Alves', 0
+
+print(pablo.__dict__)
