fd8ef3694842e2a0bb9557df6aa6c2378a7a6656
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-14 20:33:11 -0300

    new

--- Estudos/Udemy/modulos/aula_datatime.py
+++ Estudos/Udemy/modulos/aula_datatime.py
@@ -10,17 +10,29 @@
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 # Instalando o pytz
 # pip install pytz types-pytz
+# datetime.timedelta e dateutil.relativetimedelta (calculando datas)
+# Docs:
+# https://dateutil.readthedocs.io/en/stable/relativedelta.html
+# https://docs.python.org/3/library/datetime.html#timedelta-objects
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 
 data = datetime.now(timezone('America/Sao_paulo'))
-data_str_data = '2022/04/20 07:49:23'
-data_str_data = '20/04/2022'
-data_str_fmt = '%d/%m/%Y'
 
-# data = datetime(2022, 4, 20, 7, 49, 23)
-data = datetime.strptime(data_str_data, data_str_fmt)
-print(data)
+# fmt = '%d/%m/%Y %H:%M:%S'
+# data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
+# data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+# delta = timedelta(days=10, hours=2)
+
+# print(data_fim - delta)
+# print(data_fim)
+
+# delta = data_fim - data_inicio
+# print(delta.days, delta.seconds, delta.microseconds)
+# print(delta.total_seconds())
+# print(data_fim > data_inicio)
+# print(data_fim < data_inicio)
+# print(data_fim == data_inicio)
 
 print(data)
