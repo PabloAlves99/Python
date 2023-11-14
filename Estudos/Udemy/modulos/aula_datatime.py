3c3b4ae0477317d26c44186565c376ce5b433cbe
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-14 20:42:11 -0300

    Update aula_datatime.py

--- Estudos/Udemy/modulos/aula_datatime.py
+++ Estudos/Udemy/modulos/aula_datatime.py
@@ -16,10 +16,10 @@
 # https://docs.python.org/3/library/datetime.html#timedelta-objects
 
 from datetime import datetime  # , timedelta
-from pytz import timezone
+# from pytz import timezone
 
-data = datetime.now(timezone('America/Sao_paulo'))
-print(data)
+# data = datetime.now(timezone('America/Sao_paulo'))
+# print(data)
 
 # fmt = '%d/%m/%Y %H:%M:%S'
 # data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
@@ -36,3 +36,13 @@ print(data)
 # print(data_fim < data_inicio)
 # print(data_fim == data_inicio)
 
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+print(data.strftime('%d/%m/%Y'))
+print(data.strftime('%d/%m/%Y %H:%M'))
+print(data.strftime('%d/%m/%Y %H:%M:%S'))
+print(data.strftime('%Y'), data.year)
+print(data.strftime('%d'), data.day)
+print(data.strftime('%m'), data.month)
+print(data.strftime('%H'), data.hour)
+print(data.strftime('%M'), data.minute)
+print(data.strftime('%S'), data.second)
