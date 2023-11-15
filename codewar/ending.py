53130e83f4957301b25e14adb9b6bd77551acdf4
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 19:40:52 -0300

    Update ending.py

--- codewar/ending.py
+++ codewar/ending.py
@@ -1,5 +1,8 @@
 def solution(string, ending):
-    return string.endswith(ending) #O endswith()método retorna True se a string terminar com o valor especificado, caso contrário, False.
+    # O endswith()método retorna True se a string terminar com o valor
+    # especificado, caso contrário, False.
+    return string.endswith(ending)
+
 
 x = 'fsasfawsfdsadsdsa'
 y = 'sadsdsa'
