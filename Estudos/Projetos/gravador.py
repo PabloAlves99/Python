4441768184b3de07e62d0a7437bf7a9d3e676aa4
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 18:46:11 -0300

    new

--- Estudos/Projetos/gravador.py
+++ Estudos/Projetos/gravador.py
@@ -5,10 +5,10 @@ audio = pyaudio.PyAudio()
 
 stream = audio.open(
     input=True,
-    format=pyaudio.paInt16, # Formato de amplitude de 16 bits
-    channels=1, # Define quantos canal de audio 
-    rate=44000, # Basicamente é a taxa do audio
-    frames_per_buffer=1024, # tamanho dos "bloquinhos" de audio
+    format=pyaudio.paInt16,  # Formato de amplitude de 16 bits
+    channels=1,  # Define quantos canal de audio
+    rate=44000,  # Basicamente é a taxa do audio
+    frames_per_buffer=1024,  # tamanho dos "bloquinhos" de audio
 )
 
 frames = []
@@ -20,15 +20,16 @@ try:
 except KeyboardInterrupt:
     pass
 
-stream.start_stream() 
+stream.start_stream()
 stream.close()
-audio.terminate() 
-arquivo_final = wave.open("gravacao.wav", "wb") #  wb = para escrever em formato de bits
+audio.terminate()
+# wb = para escrever em formato de bits
+arquivo_final = wave.open("gravacao.wav", "wb")
 
 # configurando com as mesmas configurações passadas
-arquivo_final.setnchannels(1) 
-arquivo_final.setframerate(44000) 
-arquivo_final.setsampwidth(audio.get_sample_size(pyaudio.paInt16)) 
+arquivo_final.setnchannels(1)
+arquivo_final.setframerate(44000)
+arquivo_final.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
 
-arquivo_final.writeframes(b"".join(frames)) 
+arquivo_final.writeframes(b"".join(frames))
 arquivo_final.close()
