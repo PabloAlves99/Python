291b2e3bc830a165c97d90105205979b8cd9c665
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 19:42:18 -0300

    Update numero_telefone.py

--- codewar/numero_telefone.py
+++ codewar/numero_telefone.py
@@ -1,12 +1,15 @@
 import random
 
-def create_phone_number(n):   
+
+def create_phone_number(n):
     if len(n) != 10:
-        return('Digite apenas 10 números')
+        print('Digite exatos 10 números')
     else:
         return '({}{}{}) {}{}{}-{}{}{}{}'.format(*n)
+
+
 n = []
 for i in range(10):
-    n.append(random.randint(0,9))
+    n.append(random.randint(0, 9))
 
 print(create_phone_number(n))
