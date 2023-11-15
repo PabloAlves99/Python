3b5ba3d6b14bbaa5b653534fb6d85754cea82a54
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 13:49:48 -0300

    Update automatic.py

--- Estudos/Udemy/modulos/automatic.py
+++ Estudos/Udemy/modulos/automatic.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-service = Service(ChromeDriverManager().install()) # Instalar o ChromeDriver na versão atual
-
-navegador = webdriver.Chrome(service= service)
+# Instalar o ChromeDriver na versão atual
+service = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=service)
 navegador.get('https://www.selenium.dev/pt-br/documentation/')
