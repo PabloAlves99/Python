fcec8a558b76ab53879ea6b2b9473cadbf9d461f
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 19:38:17 -0300

    Update DNA.py

--- codewar/DNA.py
+++ codewar/DNA.py
@@ -1,12 +1,18 @@
+"""
+Atividade do CodeWar
+"""
+
 
 def dna_strand(dna):
-    reference = { "A":"T",
-                  "T":"A",
-                  "C":"G",
-                  "G":"C"
-                  }
+    """
+    Função simples para manipular DNA
+    """
+    reference = {"A": "T",
+                 "T": "A",
+                 "C": "G",
+                 "G": "C"
+                 }
     return "".join([reference[x] for x in dna])
 
-print(dna_strand('ATTGC'))
 
-    
+print(dna_strand('ATTGC'))
