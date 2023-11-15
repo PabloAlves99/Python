0e48376ca9095cdbbbf6a9b9dae18c5b5e0aa77c
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-15 16:17:28 -0300

    Create aula_manipular_arquivos.py

---
+++ Estudos/Udemy/modulos/aula_manipular_arquivos.py
@@ -0,0 +1,37 @@
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename
+import os
+import shutil
+
+HOME = os.path.abspath('')
+DESKTOP = os.path.join(HOME, 'Estudos', 'modulos')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'teste1')
+NOVA_PASTA = os.path.join(DESKTOP, 'pasta_teste')
+
+print(NOVA_PASTA)
+
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+# shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
+# # shutil.move(NOVA_PASTA, NOVA_PASTA + '_EITA')
+# shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+
+# os.makedirs(NOVA_PASTA, exist_ok=True)
+
+# for root, dirs, files in os.walk(PASTA_ORIGINAL):
+#     for dir_ in dirs:
+#         caminnho_novo_diretorio = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+#         )
+#         os.makedirs(caminnho_novo_diretorio, exist_ok=True)
+
+#     for file in files:
+#         caminho_arquivo = os.path.join(root, file)
+#         caminnho_novo_arquivo = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+#         )
+#         shutil.copy(caminho_arquivo, caminnho_novo_arquivo)
