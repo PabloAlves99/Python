e6745ddf219734085161e0a7b5ec3ecdc54ff27b
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-22 00:31:42 -0300

    up

--- Estudos/Udemy/modulos/aula_csv2.py
+++ Estudos/Udemy/modulos/aula_csv2.py
@@ -1,3 +1,9 @@
 # csv.reader e csv.DictReader
 # csv.reader lê o CSV em formato de lista
 # csv.DictReader lê o CSV em formato de dicionário
+
+from pathlib import Path
+
+CAMINHO = Path(__file__).parent / 'aula_csv.csv'
+
+print(CAMINHO)
