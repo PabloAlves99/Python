41fa0f9e6ce5a3802bd53aec5ae6706468a688de
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-22 00:45:46 -0300

    Update aula_csv2.py

--- Estudos/Udemy/modulos/aula_csv2.py
+++ Estudos/Udemy/modulos/aula_csv2.py
@@ -3,7 +3,14 @@
 # csv.DictReader lê o CSV em formato de dicionário
 
 from pathlib import Path
+import csv
 
 CAMINHO = Path(__file__).parent / 'aula_csv.csv'
 
-print(CAMINHO)
+with open(CAMINHO, 'r', encoding='utf8') as arquivo:
+    leitor = csv.DictReader(arquivo)
+
+    for linha in leitor:
+        print(linha['Nome'])
+        print(linha['Sobrenome'])
+        print(linha['Idade'])
