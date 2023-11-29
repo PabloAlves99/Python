74dd827edb4cd6ad5fafcd22e61e014ce2a4970c
Pablo Alves <PabloHAlves99@gmail.com>
2023-11-29 01:10:40 -0300

    up

--- Estudos/Udemy/POO/class_ligada.py
+++ Estudos/Udemy/POO/class_ligada.py
@@ -13,30 +13,33 @@ class Carro:
         self.nome = nome
         self._motor = None
         self._fabricante = None
-        
+
     @property
     def motor(self):
         return self._motor
-    
+
     @motor.setter
     def motor(self, motor):
         self._motor = motor
-        
+
     @property
     def fabricante(self):
         return self._fabricante
-    
+
     @fabricante.setter
     def fabricante(self, fabricante):
         self._fabricante = fabricante
 
+
 class Motor:
     def __init__(self, nome):
         self.nome = nome
-        
+
+
 class Fabricante:
     def __init__(self, nome):
         self.nome = nome
+
 
 i8 = Carro('i8')
 bmw = Fabricante('BMW')
