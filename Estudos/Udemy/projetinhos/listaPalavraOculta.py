f6475d6c1e40c6db419a6dc0b04e549e550840b6
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-02 01:05:12 -0300

    Update listaPalavraOculta.py

--- Estudos/Udemy/projetinhos/listaPalavraOculta.py
+++ Estudos/Udemy/projetinhos/listaPalavraOculta.py
@@ -2,40 +2,41 @@ import os
 
 print(" Tente completar a palavra oculta")
 PALAVRA = 'CRUZEIRO'
-palavraFormada = ['*'] * len(PALAVRA) 
+palavraFormada = ['*'] * len(PALAVRA)
 vezes = 0
 print('*' * len(PALAVRA))
 
 while True:
-    palavraCerta = ''   
+    palavraCerta = ''
     tentativa = input('\nDigite uma letra: ').upper()
-    vezes += 1  
-     
+    vezes += 1
+
     if len(tentativa) > 1:
         print('Digite apenas uma letra!!!')
         continue
-    
+
     if tentativa in palavraFormada:
         print('Você já digitou essa letra')
         continue
-    
+
     if tentativa in PALAVRA:
-        
+
         for indice, i in enumerate(PALAVRA):
-                           
+
             if i == tentativa:
-                palavraFormada[indice]= tentativa
-        
+                palavraFormada[indice] = tentativa
+
         for l in palavraFormada:
-            palavraCerta += l     
-             
+            palavraCerta += l
+
         print(f'{palavraCerta}')
-        
+
     else:
         print(f'ERROU! Não tem a letra {tentativa} nessa palavra, tente outra')
         continue
-    
+
     if '*' not in palavraFormada:
         os.system('cls')
-        print(f'\nParabens, você acertou a palavra!! \n{PALAVRA} que tem {len(PALAVRA)} letras, você acertou com {vezes} tentativas.')
+        print(
+            f'\nParabens, você acertou a palavra!! \n{PALAVRA} que tem {len(PALAVRA)} letras, você acertou com {vezes} tentativas.')
         break
