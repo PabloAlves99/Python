37c62d5a39ed81cc4db3beff4d7840bf9239ede7
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-02 01:04:46 -0300

    Update gerarCPF.py

--- Estudos/Udemy/projetinhos/gerarCPF.py
+++ Estudos/Udemy/projetinhos/gerarCPF.py
@@ -1,34 +1,36 @@
 import random
 
+
 def verificarDigi(CPF, v):
     multi = v
     somaNoveD = []
-    
+
     for digito in CPF:
         somaNoveD.append(int(digito) * multi)
         multi -= 1
         if multi == 1:
             somaNoveD = (sum(somaNoveD)) * 10
-            break       
-           
+            break
+
     resto = somaNoveD % 11
-    
+
     nDigito = 0 if resto > 9 else resto
     return str(nDigito)
 
+
 def gerarCPF():
-    
+
     CPF = ''
     for i in range(9):
         CPF += str(random.randint(0, 9))
-        
+
     v = 10
     CPF += verificarDigi(CPF, v)
     v += 1
     CPF += verificarDigi(CPF, v)
-    
-    print ('\n{}{}{}.{}{}{}.{}{}{}-{}{}'.format(*CPF))
-    
+
+    print('\n{}{}{}.{}{}{}.{}{}{}-{}{}'.format(*CPF))
+
 
 x = int(input('Quantos CPF você deseja que seja criado? '))
 for i in range(x):
