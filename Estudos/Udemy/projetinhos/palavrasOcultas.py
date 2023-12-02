ca669c1526517637b80baf9763d028225c4d0402
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-02 01:05:18 -0300

    Update palavrasOcultas.py

--- Estudos/Udemy/projetinhos/palavrasOcultas.py
+++ Estudos/Udemy/projetinhos/palavrasOcultas.py
@@ -10,30 +10,31 @@ while True:
     palavraCerta = ''
     tentativa = input('\nDigite uma letra: ').upper()
     vezes += 1
-    
+
     if len(tentativa) > 1:
         print('Digite apenas uma letra')
         continue
-    
+
     if tentativa in completando:
         print('Você já digitou essa letra')
         continue
-    
+
     if tentativa in PALAVRA:
         completando += tentativa
-        
+
         for letraSecreta in PALAVRA:
             if letraSecreta in completando:
                 palavraCerta += letraSecreta
             else:
                 palavraCerta += '*'
         print(palavraCerta)
-    
+
     else:
         print(f'ERROU! Não tem a letra {tentativa} nessa palavra, tente outra')
         continue
-    
+
     if PALAVRA == palavraCerta:
         os.system('cls')
-        print(f'\nParabens, você acertou a palavra!! \n{PALAVRA} que tem {len(PALAVRA)} letras, você acertou com {vezes} tentativas.')
+        print(
+            f'\nParabens, você acertou a palavra!! \n{PALAVRA} que tem {len(PALAVRA)} letras, você acertou com {vezes} tentativas.')
         break
