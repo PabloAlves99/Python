1c6999c77c2b9d4e94bf272304b2566e48303c9e
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-02 01:04:33 -0300

    Update calculadora.py

--- Estudos/Udemy/projetinhos/calculadora.py
+++ Estudos/Udemy/projetinhos/calculadora.py
@@ -1,4 +1,4 @@
-while True: 
+while True:
     try:
         n1 = float(input("\nDigite o primeiro número: "))
         n2 = float(input("Digite o segundo número: "))
@@ -6,14 +6,14 @@ while True:
         print("Digite apenas números, qualquer outro tipo não funcionará...")
         continue
 
-    opera = input("Digite a operação que deseja fazer (apenas +, -, / e *) ")   
-    if (opera not in '+-*/'):
+    opera = input("Digite a operação que deseja fazer (apenas +, -, / e *) ")
+    if opera not in '+-*/':
         print('Digite apenas os operadores mencionados, essa é uma calculadora simples! \n')
         continue
     if len(opera) > 1:
         print("Digite apenas um operador para fazer o calcular!")
         continue
-    
+
     if opera == '+':
         resultado = n1 + n2
         print(f'\nResultado:\n{n1} {opera} {n2} = {resultado}')
@@ -28,13 +28,14 @@ while True:
         print(f'\nResultado:\n{n1} {opera} {n2} = {resultado}')
     else:
         print('Não deveria chegar aqui \n')
-    
+
     condicao = input('\nDeseja fazer outro calculo? (sim ou não) ')
-    
+
     abreviacao = (condicao.lower() == 'n') or (condicao == 'N')
-    qualquerTamanho = (condicao.lower() == 'não') or (condicao.lower() == 'nao')
-    
+    qualquerTamanho = (condicao.lower() == 'não') or (
+        condicao.lower() == 'nao')
+
     if qualquerTamanho or abreviacao:
         break
-    
+
 print(f'\nFim do calculo')
