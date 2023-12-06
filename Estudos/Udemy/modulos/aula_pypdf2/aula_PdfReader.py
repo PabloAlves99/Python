c30cfbfc5b6f045b30c61b0eb99238a29936f077
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-06 20:23:34 -0300

    Up

--- Estudos/Udemy/modulos/aula_pypdf2/aula_PdfReader.py
+++ Estudos/Udemy/modulos/aula_pypdf2/aula_PdfReader.py
@@ -10,10 +10,10 @@ from pathlib import Path
 
 from PyPDF2 import PdfReader
 
+# Caminhos
 PASTA_RAIZ = Path(__file__).parent
 PASTA_ORIGINAIS = PASTA_RAIZ / 'pdfs_originais'
 PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos'
-
 RELATORIO_BACEN = PASTA_ORIGINAIS / 'R20230210.pdf'
 
 PASTA_NOVA.mkdir(exist_ok=True)
