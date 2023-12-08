0a7e318d4e807fd151b1b9533be650c518174209
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-08 17:11:19 -0300

    Update main.py

--- Estudos/Udemy/modulos/aula_openpyxl/main.py
+++ Estudos/Udemy/modulos/aula_openpyxl/main.py
@@ -7,3 +7,16 @@
 # manipulação de grandes quantidades de informações.
 # Instalação necessária: pip install openpyxl
 # Documentação: https://openpyxl.readthedocs.io/en/stable/
+
+students = [
+    ['Alice', 23, 8.5],
+    ['Bob', 21, 7.2],
+    ['Charlie', 24, 9.0],
+    ['David', 22, 6.8],
+    ['Eva', 20, 8.2],
+    ['Frank', 25, 9.5],
+    ['Grace', 19, 7.0],
+    ['Hank', 23, 8.8],
+    ['Ivy', 20, 7.5],
+    ['Jack', 24, 9.2]
+]
