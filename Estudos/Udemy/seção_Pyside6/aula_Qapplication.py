b82667627fd19cb981844ec5018c7430a1d2ec78
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-08 23:42:23 -0300

    Update aula_Qapplication.py

--- Estudos/Udemy/seção_Pyside6/aula_Qapplication.py
+++ Estudos/Udemy/seção_Pyside6/aula_Qapplication.py
@@ -2,3 +2,17 @@
 # QApplication -> O Widget principal da aplicação
 # QPushButton -> Um botão
 # PySide6.QtWidgets -> Onde estão os widgets do PySide6
+
+import sys
+
+from PySide6.QtWidgets import QApplication, QPushButton
+
+app = QApplication(sys.argv)
+
+botao = QPushButton('Testo do botão')
+botao.setStyleSheet('font-size: 40px;')
+botao.show()
+
+botao = QPushButton('Testo do botão')
+
+app.exec()
