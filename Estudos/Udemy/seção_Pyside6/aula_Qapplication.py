60b57d40df6c4bd240875a54ec35c99691cba01e
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-08 23:30:52 -0300

    Create aula_Qapplication.py

---
+++ Estudos/Udemy/seção_Pyside6/aula_Qapplication.py
@@ -0,0 +1,4 @@
+# QApplication e QPushButton de PySide6.QtWidgets
+# QApplication -> O Widget principal da aplicação
+# QPushButton -> Um botão
+# PySide6.QtWidgets -> Onde estão os widgets do PySide6
