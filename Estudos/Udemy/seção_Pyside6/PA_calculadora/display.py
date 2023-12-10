ff059eed4f96d208a0e1921db97384916374fe36
Pablo Alves <PabloHAlves99@gmail.com>
2023-12-10 00:55:30 -0300

    Up

--- Estudos/Udemy/seção_Pyside6/PA_calculadora/display.py
+++
@@ -1,9 +0,0 @@
-#  pylint: disable=missing-docstring
-#  pylint: disable=no-name-in-module
-#  type: ignore
-from PySide6.QtWidgets import QLineEdit
-
-
-class Display(QLineEdit):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
--- Estudos/Udemy/seção_Pyside6/PA_calculadora/main.py
+++ Estudos/Udemy/seção_Pyside6/PA_calculadora/main.py
@@ -3,11 +3,41 @@
 #  type: ignore
 import sys
 
-from main_window import MainWindow
-from display import Display
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import (QApplication, QWidget, QMainWindow, QVBoxLayout,
+                               QLineEdit,)
 from PySide6.QtGui import QIcon
-from variables import WINDOW_ICON_PATH
+from PySide6.QtCore import Qt
+from variables import (WINDOW_ICON_PATH, BIG_FONT_SIZE, TEXT_MARGIN,
+                       MINIMUM_WIDTH)
+
+
+class Display(QLineEdit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config_style()
+
+    def config_style(self):
+        self.setStyleSheet(f'font-size: {BIG_FONT_SIZE}px;')
+        self.setMinimumHeight(BIG_FONT_SIZE * 2)
+        self.setMinimumWidth(MINIMUM_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(*[TEXT_MARGIN for _ in range(4)])
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        # Configurando o layout básico
+        self.cw = QWidget()
+        self.v_layout = QVBoxLayout()
+        self.cw.setLayout(self.v_layout)
+        self.setCentralWidget(self.cw)
+        self.resize(400, 550)
+        self.setStyleSheet('background-color: #262626; color: #A6A6A6;')
+
+        self.setWindowTitle('Pablo Alves - Calculator')
+
 
 if __name__ == '__main__':
     # Cria a aplicação
@@ -22,6 +52,7 @@ if __name__ == '__main__':
     # Display
     display = Display()
     window.v_layout.addWidget(display)
+
     # Executa
     window.show()
     sys.exit(app.exec())
