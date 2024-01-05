b99938a5a57d530c7bca59de00f42d3d7401daaf
Pablo Alves <PabloHAlves99@gmail.com>
2024-01-05 19:42:38 -0300

    começando com sqlite

---
+++ Udemy/base_de_dados/one.py
@@ -0,0 +1,14 @@
+# pylint: disable=all
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.close()
+connection.close()
