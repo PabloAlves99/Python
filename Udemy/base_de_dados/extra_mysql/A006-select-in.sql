eb601c74cf15e6849e3da6865ea9c1df38246fd6
Pablo Alves <PabloHAlves99@gmail.com>
2024-02-24 11:35:23 -0300

    Update A006-select-in.sql

--- Udemy/base_de_dados/extra_mysql/A006-select-in.sql
+++ Udemy/base_de_dados/extra_mysql/A006-select-in.sql
@@ -1,4 +1,4 @@
 -- in seleciona elementos entre os valores enviados
 select * from users
-where id in (110,115,120,125,130,1000,12200,1212545)
+where id in (1,2,5,10,30,100,112,1212545)
 and first_name in ('Pablo', 'Alves');
