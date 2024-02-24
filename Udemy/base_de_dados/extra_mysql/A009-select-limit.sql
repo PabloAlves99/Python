cb9050e6a97394622c5fb0be5a2bbebbf5a07ad3
Pablo Alves <PabloHAlves99@gmail.com>
2024-02-24 11:30:48 -0300

    Update A009-select-limit.sql

--- Udemy/base_de_dados/extra_mysql/A009-select-limit.sql
+++ Udemy/base_de_dados/extra_mysql/A009-select-limit.sql
@@ -2,6 +2,6 @@
 -- offset desloca o cursor para exibir os resultados
 select id, first_name, email as uemail 
 from users
-where id between 100 and 150
+where id between 50 and 100
 order by id asc
 limit 9,3;
