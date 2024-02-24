9a89246d184e7f69ab3ce930e4286528244e402c
Pablo Alves <PabloHAlves99@gmail.com>
2024-02-24 11:29:05 -0300

    Update A011-delete.sql

--- Udemy/base_de_dados/extra_mysql/A011-delete.sql
+++ Udemy/base_de_dados/extra_mysql/A011-delete.sql
@@ -1,6 +1,6 @@
 -- Delete apaga registros da tabela
-DELETE FROM users where id = 115;
+DELETE FROM users where id = 112;
 
 -- Aviso: use SELECT para garantir que está
 -- apagando os valores corretos
-select * from users where id BETWEEN 110 and 115;
+select * from users where id BETWEEN 110 and 112;
