2e36856c6ab90775a8cc0480dc381603da071320
Pablo Alves <PabloHAlves99@gmail.com>
2024-02-24 11:31:17 -0300

    Update A008-select-order.sql

--- Udemy/base_de_dados/extra_mysql/A008-select-order.sql
+++ Udemy/base_de_dados/extra_mysql/A008-select-order.sql
@@ -4,5 +4,5 @@
 -- order by id asc, first_name desc (prioriza o id)
 select id, first_name, email as uemail 
 from users
-where id between 100 and 150
+where id between 50 and 100
 order by first_name desc;
