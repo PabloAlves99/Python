671192d2c35ab1459956d2bdec24564b2b795763
Pablo Alves <PabloHAlves99@gmail.com>
2024-02-24 11:29:34 -0300

    Update A010-insert-select.sql

--- Udemy/base_de_dados/extra_mysql/A010-insert-select.sql
+++ Udemy/base_de_dados/extra_mysql/A010-insert-select.sql
@@ -4,6 +4,6 @@ insert into profiles
 (bio, description, user_id)
 select 
 concat('Bio de ', first_name), 
-concat('Description de', ' ', first_name), 
+concat('Description de ', first_name), 
 id 
 from users;
