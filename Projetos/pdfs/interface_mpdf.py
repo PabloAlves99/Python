c0ce1bc63b1f69177f366c5635fe4af6d179febc
Pablo <PabloHAlves99@gmail.com>
2024-05-28 23:52:18 -0300

    Update interface_mpdf.py

--- Projetos/pdfs/interface_mpdf.py
+++ Projetos/pdfs/interface_mpdf.py
@@ -24,7 +24,7 @@ class PDFProcessorApp:
                                highlightthickness=3)
         self.top_frame.place(relx=0.02, rely=0.02,
                              relwidth=0.96, relheight=0.46)
-        
+
         self.bottom_frame = Frame(self.root, bd=4, bg='#A3C9D9',
                                   highlightbackground='#A9CDDA',
                                   highlightthickness=3)
