07b93ff19ddab61d889126aad08b2a0262bca5e6
Pablo <PabloHAlves99@gmail.com>
2024-06-13 20:08:20 -0300

    Update organizador_de_arquivos.py

--- Projetos/organizador_de_arquivos.py
+++ Projetos/organizador_de_arquivos.py
@@ -9,34 +9,34 @@ DATA_ATUAL = datetime.now().strftime("%Y%m%d")
 caminho = askdirectory(title="Selecione uma pasta")
 lista_arquivos = os.listdir(caminho)
 extensoes = {
-    "oa_imagens": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".ico"],
-    "oa_DOCX": [".docx", ".doc"],
-    "oa_planilhas": [".xlsx", ".xls", ".csv", ".ods"],
-    "oa_pdfs": [".pdf", ".PDF"],
-    "oa_texto": [".txt", ".rtf", ".tex",],
-    "oa_apresentacoes": [".pptx", ".ppt", ".md"],
-    "oa_audio": [".mp3", ".wav", ".ogg", ".flac"],
-    "oa_video": [".mp4", ".avi", ".mov", ".wmv", ".m4v", ".REC"],
-    "oa_executaveis": [".exe", ".bat", ".sh", ".msi"],
-    "oa_compactados": [".zip", ".rar", ".tar.gz"],
-    "oa_Python": [".py"],
-    "oa_C": [".c", ".h"],
-    "oa_C++": [".cpp", ".hpp"],
-    "oa_Java": [".java", ".class"],
-    "oa_JavaScript": [".js"],
-    "oa_HTML": [".html", ".htm"],
-    "oa_CSS": [".css"],
-    "oa_Ruby": [".rb"],
-    "oa_PHP": [".php"],
-    "oa_Go": [".go"],
-    "oa_TypeScript": [".ts"],
-    "oa_SQL": [".sql"],
-    "oa_Shell": [".sh"],
-    "oa_Perl": [".pl"],
-    "oa_R": [".r"],
-    "oa_Assembly": [".asm"],
-    "oa_outros": [".bibtex", ".apk", ".xml"],
-    "JNLP": [".jnlp",]
+    "imagens": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".ico"],
+    "DOCX": [".docx", ".doc"],
+    "planilhas": [".xlsx", ".xls", ".csv", ".ods"],
+    "pdfs": [".pdf", ".PDF"],
+    "texto": [".txt", ".rtf", ".tex",],
+    "apresentacoes": [".pptx", ".ppt", ".md"],
+    "audio": [".mp3", ".wav", ".ogg", ".flac"],
+    "video": [".mp4", ".avi", ".mov", ".wmv", ".m4v", ".REC"],
+    "executaveis": [".exe", ".bat", ".sh", ".msi"],
+    "compactados": [".zip", ".rar", ".tar.gz"],
+    "Python": [".py"],
+    "C": [".c", ".h"],
+    "C++": [".cpp", ".hpp"],
+    "Java": [".java", ".class"],
+    "JavaScript": [".js"],
+    "HTML": [".html", ".htm"],
+    "CSS": [".css"],
+    "Ruby": [".rb"],
+    "PHP": [".php"],
+    "Go": [".go"],
+    "TypeScript": [".ts"],
+    "SQL": [".sql"],
+    "Shell": [".sh"],
+    "Perl": [".pl"],
+    "R": [".r"],
+    "Assembly": [".asm"],
+    "JNLP": [".jnlp",],
+    "outros": [".bibtex", ".apk", ".xml", ".jar"],
 }
 for arquivo in lista_arquivos:
     nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
@@ -44,7 +44,8 @@ for arquivo in lista_arquivos:
     for pasta in extensoes:  # pylint: disable=C0206
 
         if extensao in extensoes[pasta]:
-            NOVO_CAMINHO = f'{caminho}/{pasta}/{DATA_ATUAL}'
+            NOVO_CAMINHO = f'{caminho}/organizador_de_arquivos/'\
+                f'{pasta}/{DATA_ATUAL}'
 
             if not os.path.exists(NOVO_CAMINHO):
                 os.makedirs(NOVO_CAMINHO)
